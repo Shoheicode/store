00b30cff195f3a9c961b23dff119db00d6af033d
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:42:57 -0700

    updated navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Container from '../global/Container'
+import Logo from './Logo'
+import NavSearch from './NavSearch'
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <Container className='flex flex-col sm:flex-row sm: justify-between gap-4 sm:items-center flex-wrap py-8'>
+        <Logo/>
+        <NavSearch/>
+        <div className='flex gap-4 item-center'>
+            
+        </div>
+    </Container>
   )
 }
