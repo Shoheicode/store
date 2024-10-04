e059b570fe58be60af792e86dd699fe48c282562
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:40:24 -0700

    updated container

--- components/global/Container.tsx
+++ components/global/Container.tsx
@@ -7,8 +7,8 @@ export default function Container(
         className?: string;}
     ) {
     return (
-        <div className={cn('mx-auto')}>
-            Container
+        <div className={cn('mx-auto max-w-6xl xl:max-w-7xl px-8', className)}>
+            {children}
         </div>
     )
 }
