cda5d5febbc1532d3eb409013cb1b81005b6fbb1
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:53:35 -0700

    updated code

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { FaRegFileCode } from "react-icons/fa";
 
 export default function Logo() {
   return (
-    <div>Logo</div>
+    <Button>
+        <Link href='/'>
+            <FaRegFileCode className='w-6 h-6'/> 
+        </Link>
+    </Button>
   )
 }
