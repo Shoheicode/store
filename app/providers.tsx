144ab3103e11abb5082ca417c38f3d4d6174eff4
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 10:09:10 -0700

    updated providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Providers({children} : {children:React.ReactNode}) {
   return (
-    <div>Providers</div>
+    <>
+        {children}
+    
+    </>
     
   )
 }
