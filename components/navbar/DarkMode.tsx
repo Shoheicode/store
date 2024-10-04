e34d881b35e7fc9387ae5e90dcc16c2d97243182
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 10:35:09 -0700

    updated

--- components/navbar/DarkMode.tsx
+++ components/navbar/DarkMode.tsx
@@ -17,7 +17,7 @@ export default function DarkMode() {
                     className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0'
                 />
                 <MoonIcon
-                    className='h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100'
+                    className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:-rotate-0 dark:scale-100'
                 />
                 <span className='sr-only'>Toggle theme</span>
             </Button>
@@ -29,9 +29,9 @@ export default function DarkMode() {
             <DropdownMenuItem onClick={() => setTheme('dark')}>
                 Dark
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme('system')}>
+            {/* <DropdownMenuItem onClick={() => setTheme('system')}>
                 System
-            </DropdownMenuItem>
+            </DropdownMenuItem> */}
         </DropdownMenuContent>
     </DropdownMenu>
   )
