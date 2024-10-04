0421b004f1de18ee902b4f2cd37db15c6a08a762
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:44:04 -0700

    updated buttons

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-export default function CardButton() {
+export default function CartButton() {
   return (
-    <div>CardButton</div>
+    <div>CartButton</div>
   )
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Container from '../global/Container'
 import Logo from './Logo'
 import NavSearch from './NavSearch'
-import CardButton from './CartButton'
+import CartButton from './CartButton'
 
 export default function Navbar() {
   return (
@@ -10,7 +10,7 @@ export default function Navbar() {
         <Logo/>
         <NavSearch/>
         <div className='flex gap-4 item-center'>
-            <CardButton/>
+            <CartButton/>
         </div>
     </Container>
   )
