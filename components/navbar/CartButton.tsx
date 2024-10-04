3333ad2d21b2a0b2ed3d5606405dbdb6ba9f742c
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 10:01:03 -0700

    add tailwind css

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -3,13 +3,18 @@ import { Button } from '../ui/button'
 import Link from 'next/link'
 import { CiShoppingCart } from "react-icons/ci";
 
-export default function CartButton() {
+export default async function CartButton() {
     // temp
     const numItemsInCard = 9
     return (
-        <Button asChild>
-            <Link href='/'>
+        <Button asChild variant='outline' size='icon'
+        className='flex justify-center items-center relative'
+        >
+            <Link href='/cart'>
              <CiShoppingCart />
+             <span>
+
+             </span>
             </Link>
         </Button>
     )
