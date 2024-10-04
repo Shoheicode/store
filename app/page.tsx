35b6ce391c050ed3b41e77dc5e2bcea8f8dc3428
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:23:08 -0700

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage(){
   return (
-    <div>Home Page</div>
+    <div>
+      <h1 className="text-3xl">Home Page</h1>
+      <Button variant='default'></Button>
+    </div>
   )
 }
 
