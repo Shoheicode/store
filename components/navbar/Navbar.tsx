b88e1464b855a603c1cf72e5b30bb8c69b161952
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:51:27 -0700

    updated package

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -8,14 +8,16 @@ import LinksDropdown from './LinksDropdown'
 
 export default function Navbar() {
   return (
-    <Container className='flex flex-col sm:flex-row sm: justify-between gap-4 sm:items-center flex-wrap py-8'>
-        <Logo/>
-        <NavSearch/>
-        <div className='flex gap-4 item-center'>
-            <CartButton/>
-            <DarkMode/>
-            <LinksDropdown/>
-        </div>
-    </Container>
+    <nav className='border-b'>
+        <Container className='flex flex-col sm:flex-row sm: justify-between gap-4 sm:items-center flex-wrap py-8'>
+            <Logo/>
+            <NavSearch/>
+            <div className='flex gap-4 item-center'>
+                <CartButton/>
+                <DarkMode/>
+                <LinksDropdown/>
+            </div>
+        </Container>
+    </nav>
   )
 }
