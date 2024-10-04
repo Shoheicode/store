c689f177d760d442742dc66dccf7d5a1e413f652
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-04 09:29:41 -0700

    added many components

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ function HomePage(){
   return (
     <div>
       <h1 className="text-3xl">Home Page</h1>
-      <Button variant='default'></Button>
+      <Button variant='outline' size='lg' className="capitalize m-8">
+        click me
+      </Button>
     </div>
   )
 }
