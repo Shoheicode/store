d1851cd0cada3c7927131cae6b0073125302215f
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-09 14:14:27 -0700

    updated

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -8,7 +8,8 @@ import {
 } from '@/components/ui/dropdown-menu'
 import {LuAlignLeft} from 'react-icons/lu'
 import Link from 'next/link'
-
+import {Button} from '../ui/button'
+import {links} from '@/utils/links'
 
 export default function LinksDropdown() {
   return (
