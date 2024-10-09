5d473d0eb41078df4808ada84cb042507c063612
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-09 14:13:55 -0700

    added updated links

--- components/navbar/LinksDropdown.tsx
+++ components/navbar/LinksDropdown.tsx
@@ -6,6 +6,9 @@ import {
   DropdownMenuTrigger,
   DropdownMenuSeparator
 } from '@/components/ui/dropdown-menu'
+import {LuAlignLeft} from 'react-icons/lu'
+import Link from 'next/link'
+
 
 export default function LinksDropdown() {
   return (
