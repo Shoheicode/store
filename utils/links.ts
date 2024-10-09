ff82763333ebf90c309f8081faba652eef806316
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-09 14:09:23 -0700

    added product

--- utils/links.ts
+++ utils/links.ts
@@ -6,7 +6,7 @@ type NavLink = {
 export const links: NavLink[] = [
    {href: "/", label: "home"},
    {href: "/about", label: "home"},
-   {href: "/", label: "home"},
+   {href: "/products", label: "home"},
    {href: "/", label: "home"},
    {href: "/", label: "home"},
    {href: "/", label: "home"}, 
