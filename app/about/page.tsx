c9ffcb1dafcfcc7ef464b6992c96a1b197fe5de6
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-16 21:11:55 -0700

    updated value

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,11 @@
 import db from '@/utils/db'
 
 async function AboutPage(){
+    const profile = await db.testProfile.create({
+      data:{
+        name: "RANDOM NAME"
+      }
+    })
     return (
       <div>About Page</div>
     )
