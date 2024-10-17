0c4fc44ffd2286a8d1f39eb5ccdf29fc6faa0116
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-16 21:34:02 -0700

    return about page to normal

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,22 +1,6 @@
-import db from '@/utils/db'
-
-async function AboutPage(){
-    const profile = await db.testProfile.create({
-      data:{
-        name: "RANDOM NAME"
-      }
-    })
-
-    const user = await db.testProfile.findMany();
+function AboutPage(){
     return (
-      <div>
-        {user.map((user) => {
-          return <h2 key={user.id} className='text-2xl font-bold'>
-            {user.name}
-          </h2>
-        })}
-
-      </div>
+      <div>About Page</div>
     )
   }
   
