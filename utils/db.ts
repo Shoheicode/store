f3deb6c25a5eb98757d695b1258ef31e8b43715c
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-16 20:56:39 -0700

    updated db file

--- utils/db.ts
+++ utils/db.ts
@@ -4,3 +4,11 @@ const prismaClientSingleton = () =>{
     return new PrismaClient()
 }
 
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClientSingleton | undefined
+}
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+
