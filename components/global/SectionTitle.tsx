8dcbdd61cd5bad75fd701a748c6a8e40a5c441ea
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-18 10:59:55 -0700

    updated h2 text

--- components/global/SectionTitle.tsx
+++ components/global/SectionTitle.tsx
@@ -3,7 +3,11 @@ import { Separator } from '@/components/ui/separator'
 
 function SectionTitle({text} : {text:string}) {
   return (
-    <div>SectionTitle</div>
+    <div>
+        <h2 className='text-3xl font-medium tracking-wider capitalize mb-8'>
+
+        </h2>
+    </div>
     
   )
 }
