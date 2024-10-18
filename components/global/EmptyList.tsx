aa171c9eee4f9e1562a84180ce4589b2ff075aee
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-18 11:02:45 -0700

    updated empty list

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,8 +1,11 @@
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 function EmptyList({heading = 'No items found.', className} : {heading?:string, className?:string}) {
   return (
-    <div>EmptyList</div>
+    <h2 className={cn('text-xl', className)}>
+        
+    </h2>
   )
 }
 
