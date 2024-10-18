87afb3d7e57d59f186501db05a51de05596bd952
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-18 11:07:12 -0700

    added a fetch product function

--- utils/actions.ts
+++ utils/actions.ts
@@ -4,7 +4,15 @@ export const fetchFeaturedProducts = async () =>{
    const products = await db.product.findMany({
     where:{
         featured: true,
-    }
+    },
    })
    return products
+}
+
+export const fetchAllProducts = () =>{
+    return db.product.findMany({
+        orderBy:{
+            createdAt:'desc'
+        }
+    })
 }
