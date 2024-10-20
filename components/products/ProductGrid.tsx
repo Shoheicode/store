1138813dbcd813f2d179cf458ba47237ef9b504c
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-19 18:57:38 -0700

    updated to adjust dollar amount

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Product } from '@prisma/client'
+import { formatCurrency } from '@/utils/format'
 
 function ProductGrid({products}:{products: Product[]}) {
   return (
     <div className='pt-12 grid gap-4 md:grid-cols-2'>
       {products.map((product)=>{
-        
+        const {name,price,image} = product
+        const productID = product.id
+        const dollarAmount = formatCurrency(price)
         return "hello"
       })
       }
