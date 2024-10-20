eb8c6963afeeec4f5cb38f5d7a9851e36c1490e2
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-19 18:56:55 -0700

    added product map

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -3,7 +3,13 @@ import { Product } from '@prisma/client'
 
 function ProductGrid({products}:{products: Product[]}) {
   return (
-    <div>ProductGrid</div>
+    <div className='pt-12 grid gap-4 md:grid-cols-2'>
+      {products.map((product)=>{
+        
+        return "hello"
+      })
+      }
+    </div>
   )
 }
 
