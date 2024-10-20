66a94a6edbecee03e3c9692c431d20dcf6caeb18
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-19 18:54:36 -0700

    updated product grid to actually work

--- components/products/ProductGrid.tsx
+++ components/products/ProductGrid.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Product } from '@prisma/client'
 
-function ProductGrid({products}:{products:[]}) {
+function ProductGrid({products}:{products: Product[]}) {
   return (
     <div>ProductGrid</div>
   )
