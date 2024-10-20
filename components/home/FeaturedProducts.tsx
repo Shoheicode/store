ebe66b4c74fbdfc9c250fb526f20cb0a0ebaf008
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-19 18:42:41 -0700

    added products

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,6 +1,10 @@
+import { fetchAllProducts } from '@/utils/actions'
 import React from 'react'
+import EmptyList from '../global/EmptyList'
 
 async function FeaturedProducts() {
+  const products = await fetchAllProducts()
+  if(products.length == 0) return <EmptyList/>
   return (
     <div>FeaturedProducts</div>
   )
