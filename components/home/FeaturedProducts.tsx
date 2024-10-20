180dc5f4aee8b0004696fd47c0f5b444746811bf
ShoheiCode <ucodejasonirie@gmail.com>
2024-10-19 18:44:12 -0700

    updated feature products

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,13 +1,16 @@
 import { fetchAllProducts } from '@/utils/actions'
 import React from 'react'
 import EmptyList from '../global/EmptyList'
+import SectionTitle from '../global/SectionTitle'
+import ProductGrid from '../products/ProductGrid'
 
 async function FeaturedProducts() {
   const products = await fetchAllProducts()
   if(products.length == 0) return <EmptyList/>
   return (
-    <section>
-      
+    <section className='pt-24'>
+      <SectionTitle text='featured products'/>
+      <ProductGrid />
     </section>
   )
 }
